7ffcfc72684680f1d5f0d836116598058ee18251
Sweksha Sinha <chrahul5@localhost>
2023-02-24 16:47:07 -0600

    Add client to issue concurrent close ops

--- unreliablefs/durability_tests/client.py
+++ unreliablefs/durability_tests/client.py
@@ -1,7 +1,7 @@
 # importing os module 
 import os
   
-target_file_path = "/users/chrahul5/bnfs/unreliablefs/build/unreliablefs/mnt/am_i_durable.txt"
+target_file_path = "/users/chrahul5/sweksha/bnfs/unreliablefs/build/unreliablefs/mnt/am_i_durable.txt"
 # target_file_path = "am_i_durable.txt"
 
 # Creates a new file.
@@ -11,11 +11,11 @@ line1 = str.encode("this is line 1")
 line2 = str.encode("\nthis is line 2")
 
 # W1
-numBytes1 = os.write(fd, line1)
-print("I have writted {} bytes".format(numBytes1))
+#numBytes1 = os.write(fd, line1)
+#print("I have writted {} bytes".format(numBytes1))
 
 # W2
-numBytes2 = os.write(fd, line2)
-print("I have writted {} bytes".format(numBytes2))
+#numBytes2 = os.write(fd, line2)
+#print("I have writted {} bytes".format(numBytes2))
 
-os.close(fd)
+#os.close(fd)
